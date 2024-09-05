3d979d139705adca55ad02377b18f2b9c89254f6
ddf172 <jakubddfadamski@gmail.com>
2024-09-05 04:07:52 +0200

    Implemented test_crossover_elevators

--- Tests/test_Crossover.py
+++ Tests/test_Crossover.py
@@ -2,6 +2,7 @@ import pytest
 from Algorithm.Crossover import Crossover
 from Settings.Settings import Settings
 from Objects.Elevator import AlgorithmElevator
+from Algorithm.Tabu import Tabu
 
 
 @pytest.fixture
@@ -21,6 +22,11 @@ def prepare_crossover_objects():
     offspring1 = AlgorithmElevator(0, 0)
     offspring2 = AlgorithmElevator(0, 0)
     return elevator1, elevator2, offspring1, offspring2
+
+
+@pytest.fixture
+def tabu():
+    return Tabu([], 0, 0)
 
 
 def prepare_crossover_objects_paths(elevator1, elevator2, offspring1, offspring2, elevator1_path, elevator2_path):
@@ -86,3 +92,28 @@ def test_append_offspring_elevator_move(crossover, settings, prepare_crossover_o
     crossover.append_offspring_elevator_move(elevator1, elevator2, offspring1, offspring2, 0)
     assert offspring1.state.path[0] in set(settings.path.path_possible_moves[1]) - {1}
     assert offspring2.state.path[0] in set(settings.path.path_possible_moves[0]) - {0}
+
+
+def test_crossover_elevators(crossover, settings, prepare_crossover_objects, tabu):
+    elevator1, elevator2, offspring1, offspring2 = prepare_crossover_objects
+
+    path1 = [0] * settings.path.path_length
+    path2 = [0] * settings.path.path_length
+    prepare_crossover_objects_paths(elevator1, elevator2, offspring1, offspring2, path1, path2)
+
+    offspring1, offspring2 = crossover.crossover_elevators(elevator1, elevator2, offspring1, offspring2)
+    assert offspring1.state.path == [0] * settings.path.path_length
+    assert offspring2.state.path == [0] * settings.path.path_length
+
+    path1 = tabu.generate_new_path()
+    path2 = tabu.generate_new_path()
+    prepare_crossover_objects_paths(elevator1, elevator2, offspring1, offspring2, path1, path2)
+
+    offspring1, offspring2 = crossover.crossover_elevators(elevator1, elevator2, offspring1, offspring2)
+
+    for i in range(settings.path.path_length):
+        if elevator1.state.path[i] == elevator2.state.path[i]:
+            assert offspring1.state.path[i] == offspring2.state.path[i] == elevator1.state.path[i]
+        else:
+            assert offspring1.state.path[i] in set(settings.path.path_possible_moves[path1[i]]) - {path1[i]}
+            assert offspring2.state.path[i] in set(settings.path.path_possible_moves[path2[i]]) - {path2[i]}
