6132821806224437f41682f25dd315727aac95c9
ddf172 <jakubddfadamski@gmail.com>
2024-09-12 05:24:04 +0200

    Added test_move_person

--- Tests/test_EvaluatorPeopleManager.py
+++ Tests/test_EvaluatorPeopleManager.py
@@ -61,3 +61,22 @@ def test_remove_person(evaluator_people_manager):
     assert person.current_affiliation is None
     assert evaluator_people_manager.containers[None].count == 1
 
+
+def test_move_person(evaluator_people_manager):
+    person = AlgorithmPerson(0, 1, None, None)
+    evaluator_people_manager.add_person(person, None)
+    evaluator_people_manager.move_person(person, None, 0)
+    assert person.id in evaluator_people_manager.containers[0].floors[person.destination]
+    assert person.current_affiliation == 0
+    assert person.original_affiliation is None
+    assert evaluator_people_manager.containers[0].count == 1
+    assert person in evaluator_people_manager.moved_elevator_people
+
+    person = AlgorithmPerson(0, 1, 0, 0)
+    evaluator_people_manager.add_person(person, 0)
+    evaluator_people_manager.move_person(person, 0, None)
+    assert person.id in evaluator_people_manager.containers[None].floors[person.start_pos]
+    assert person.current_affiliation is None
+    assert person.original_affiliation == 0
+    assert evaluator_people_manager.containers[None].count == 1
+    assert person in evaluator_people_manager.moved_elevator_people
