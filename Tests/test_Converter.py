6218a26d873741782fa5430f446b2d04d2e953cd
ddf172 <jakubddfadamski@gmail.com>
2024-09-17 10:27:49 +0200

    Implemented test_convert_person and helper function assert_person_conversion

--- Tests/test_Converter.py
+++ Tests/test_Converter.py
@@ -1,7 +1,25 @@
 import pytest
-from Managers.Converter import *
+import Managers.Converter as Converter
 from Managers.EvaluatorPeopleManager import EvaluatorPeopleManager
 from Managers.SystemPeopleManager import SystemPeopleManager
 from Managers.PeopleManager import PeopleContainer
 from Objects.Person import AlgorithmPerson, Person
 
+
+def assert_person_conversion(person, algorithm_person, where):
+    assert algorithm_person.start_pos == person.start_pos
+    assert algorithm_person.destination == person.destination
+    assert algorithm_person.current_affiliation == where
+    assert algorithm_person.original_affiliation == where
+
+
+def test_convert_person():
+    person = Person(0, 1)
+    algorithm_person = Converter._convert_person(person, None)
+    assert isinstance(algorithm_person, AlgorithmPerson)
+    assert_person_conversion(person, algorithm_person, None)
+
+    person = Person(0, 1)
+    algorithm_person = Converter._convert_person(person, 0)
+    assert isinstance(algorithm_person, AlgorithmPerson)
+    assert_person_conversion(person, algorithm_person, 0)
