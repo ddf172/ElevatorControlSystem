5b89399cf9606927d2cd77f333dd131f5b2cdd84
ddf172 <jakubddfadamski@gmail.com>
2024-09-17 07:24:11 +0200

    Moved system_people_manager and evaluator_people_manager fixtures from corresponding files to confitest

---
+++ Tests/test_Converter.py
@@ -0,0 +1,7 @@
+import pytest
+from Managers.Converter import *
+from Managers.EvaluatorPeopleManager import EvaluatorPeopleManager
+from Managers.SystemPeopleManager import SystemPeopleManager
+from Managers.PeopleManager import PeopleContainer
+from Objects.Person import AlgorithmPerson, Person
+
